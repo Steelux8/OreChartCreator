dff769ec551667be023c007540e2d09bff7b1cf5
Daniel Mendes <70096037+Steelux8@users.noreply.github.com>
2026-09-02 21:42:19 +0100

    First setup for releases
    - Added a helper method to setup GraphViz;
    - Currently setting up a build with PyInstaller, build is successful but executable fails due to lacking input files that were not specified in the build command.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QApplication
 import sys
+import os
 
 # Import the dialog and your main app class
 from modpack_selection_dialog import ModpackSelectionDialog # pyright: ignore[reportMissingImports]
@@ -7,7 +8,17 @@ from ore_chart_creator import OreChartApp  # Your main window class
 
 MODPACKS = ["Enigmatica", "GTNH"]
 
+def setup_bundled_graphviz():
+    base_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    
+    graphviz_bin = os.path.join(base_dir, "graphviz_bin")
+    
+    if os.path.exists(graphviz_bin):
+        os.environ["PATH"] = graphviz_bin + os.pathsep + os.environ["PATH"]
+
 def main():
+    setup_bundled_graphviz()
+
     app = QApplication(sys.argv)
 
     # Show modpack selection dialog
